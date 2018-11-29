3a63d0d50ad0d304bda8960c210dd9be85e15705
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2018-11-29 18:12:22 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import faker from 'faker'
+import CommentDetail from './CommentDetail'
+import ApprovalCard from "./ApprovalCard"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return(
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                Warning
+                </div>
+                Are you sure you want to do this?
+            </ApprovalCard>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+            <ApprovalCard>
+                <CommentDetail  
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    textContent="Nice Blog" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 5:45PM" 
+                    textContent="Beatiful Blog" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Today at 6:45PM" 
+                    textContent="fantastic Blog" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    )
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));
